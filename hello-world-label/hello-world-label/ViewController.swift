94243ceef5975f0733b8ee62fda5e0957bbc4bc0
sheyla fuentes <sgf763@mail.missouri.edu>
2019-02-01 20:33:58 -0600

    hello world label

---
+++ hello-world-label/hello-world-label/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hello-world-label
+//
+//  Created by Sheyla Astete on 2/1/19.
+//  Copyright © 2019 sheyla Fuentes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
